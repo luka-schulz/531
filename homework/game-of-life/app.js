1d1154d6fdde296bba3c106a91f32e4c5a911a3a
Luka Schulz <giggles14@verizon.net>
2018-01-23 14:58:28 -0500

    Completed implementation of the animation loop

--- homework/game-of-life/app.js
+++ homework/game-of-life/app.js
@@ -4,7 +4,20 @@
   let currentGrid = [];
   let nextGrid = [];
   
-  let gridSize = 10;
+  const gridSize = 50;
+  
+  // [numNeighbors, state]
+  const GameRules = [
+    [0, 0],
+    [1, 0], // die from underpopulation
+    [2, 1], // live
+    [3, 1], // live (reporduce)
+    [4, 0], // die from overpopulation
+    [5, 0],
+    [6, 0],
+    [7, 0],
+    [8, 0]
+  ]
   
   const app = {
     canvas: null,
@@ -18,16 +31,16 @@
       
       window.onresize = this.fullScreenCanvas.bind( this );
       
-      requestAnimationFrame( this.draw );
-      
       for( let y = 0; y < gridSize; y++ ) {
         currentGrid[y] = []
         nextGrid[y] = [];
         for( let x = 0; x < gridSize; x++ ) {
-          currentGrid[y][x] = Math.random() > .5 ? 1 : 0;
+          currentGrid[y][x] = this.randState();
           nextGrid[y][x] = 0;
         }
-      }
+      };
+      
+      requestAnimationFrame( this.draw );
     },
     
     fullScreenCanvas() {
@@ -37,29 +50,33 @@
     
     // update your simulation here
     animate() {
-//      let swap = currentGrid;
-//      currentGrid = nextGrid;
-//      nextGrid = swap;
-//      
-//      for( let i = 0; i < gridSize; i++ ) {
-//        let row = swap[i];
-//        let yPos =  i * cellHeight;
-//
-//        for( let j = 0; j < gridSize; j++ ) {
-//          let cell = row[j];
-//
-//          if( cell === 1 ) {
-//            let xPos = j * cellWidth;
-//
-//            this.ctx.fillStyle = "#FAFAFA";
-//            this.ctx.fillRect( xPos, yPos, cellWidth, cellHeight );
-//          }
-//        }
-//      }
+      let cellWidth = this.canvas.width / gridSize;
+      let cellHeight = this.canvas.height / gridSize;
+      
+      let swap = currentGrid;
+      
+      for( let y = 0; y < gridSize; y++ ) {
+        let row = currentGrid[y];
+        let yPos =  y * cellHeight;
+
+        for( let x = 0; x < gridSize; x++ ) {
+          let cell = row[x];
+          
+          let numLiving = this.getCellNeighbors( currentGrid, x, y)
+            .filter( cell => currentGrid[ cell[1] ][ cell[0] ] === 1 ) // only keep living cells
+            .length;
+          
+          nextGrid[y][x] = this.cellEvolve( cell, numLiving );
+        }
+      }
+      
+      
+      currentGrid = nextGrid;
+      nextGrid = swap;
     },
     
     draw() {
-      // requestAnimationFrame( this.draw );
+      requestAnimationFrame( this.draw );
       this.animate();
       
       // draw to your canvas here
@@ -84,29 +101,50 @@
           }
         }
       }
-      this.findNeighbors( currentGrid, 4, 5 )
     },
     
-    findNeighbors( myArray, i, j ) {  
-      var rowLimit = myArray[0].length - 1;
-      var columnLimit = myArray.length - 1;
+    getCellNeighbors( myArray, i, j ) {  
+      let rowLimit = myArray[0].length - 1;
+      let columnLimit = myArray.length - 1;
+      let neighbors = [];
 
       // Math.max() is being used to find the lower bound
       // and ensure that the search does not go bellow the
       // lower limit of 0
       
       // Math.min() is being used to find the upper bound
-      // and ensure that thh search does not exceed either
+      // and ensure that the search does not exceed either
       // the row length or the column length
       for( let y = Math.max( 0, j-1 ); y <= Math.min( j+1, rowLimit ); y++ ) {
         for( let x = Math.max( 0, i-1 ); x <= Math.min( i+1, columnLimit ); x++ ) {
-          // if x === i && y ====j this statment will return false
-          // this ignores the cell which we are searching from (the centeral cell)
+          // if x === i && y ====j this statement will return false
+          // this ignores the cell which we are searching from (i.e. the central cell)
           if( x !== i || y !== j ) { 
-            console.log(x + ", " + y + ": " + myArray[y][x]);
+            neighbors.push([x, y])
           }
         }
       }
+      
+      return neighbors;
+    },
+    
+    cellEvolve( state, neighbors ) {
+//      let rule = GameRules.find( rule => rule[0] === neighbors );
+//      
+//      return rule[1];
+      if( state === 0 && neighbors === 3) {
+        return 1;
+      }
+      else if( state === 1 && (neighbors < 2  || neighbors > 3) ) {
+        return 0;
+      }
+      else {
+        return state;
+      }
+    },
+    
+    randState() {
+      return Math.random() > .5 ? 1 : 0;
     }
   }
   
