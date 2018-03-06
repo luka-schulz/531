951a7b78e1011f3ad3d1eb8216c8b76ce0726fa2
Luka Schulz <giggles14@verizon.net>
2018-03-06 09:13:19 -0500

    Made texture map

--- homework/12_lorenz-attractor/js/app.js
+++ homework/12_lorenz-attractor/js/app.js
@@ -65,7 +65,7 @@ window.app = {
     if( position.adamMode ) {
       material = new THREE.MeshPhongMaterial( {
         alphaMap: new THREE.TextureLoader().load( "textures/adamS-alpha.png" ),
-        emissive: new THREE.TextureLoader().load( "textures/adamS.png" ),
+        map: new THREE.TextureLoader().load( "textures/adamS.png" ),
       } );
       
       material.transparent = true
